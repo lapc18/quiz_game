bffa10d0b28e443b923b3a232dcf2c28c4c46ea9
Radhalvy Bernabel <radhalvybernabel@gmail.com>
2021-07-29 16:33:46 -0400

    Created a new class called Question in a new file and used it in the game file

--- game.py
+++ game.py
@@ -1,21 +1,18 @@
-questions = [
-                "1) What is the color of the sky?\na) Red  b) Blue  c) Yellow",
-                "2) What is the color of the grass?\na) Green  b) Purple  c) Black",
-                "3) How many fingers do humans have on each hand?\na) 2  b) 4  c) 5"
-            ]
-answers =   [
-                "Blue",
-                "Green",
-                "5"
-            ]
-            
+from classes import Question
 
+questions = [Question("""\nWhat is the color of the sky?
+                       \na) Red b) Blue c) Yellow""", "BLUE"),
+             Question("""\nWhat is the color of the grass?
+                       \na) Green b) Purple c) Black""", "GREEN"),
+             Question("""\nHow many fingers do humans have on each hand?
+                       \na) 2 b) 4 c) 5""", "5")
+             ]
 points = 0
-for question in questions:
-    print(question)
+for item in questions:
+    print(item.prompt)
     user_answer = input("\nAnswer: ")
-    if user_answer in answers:
+    if user_answer.upper() == item.answer:
         points += 1
     else:
         points = points
-print(f"You got {points} points.")
+print(f"\nYou got {points} points.")
